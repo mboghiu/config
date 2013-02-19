586beb6091eeaaa77698a474ffa13a04b65a6f3f
Maria Boghiu <maria.boghiu@flextrade.com>
2013-02-19 14:52:16 +0000

    .inputrc and bash functions

---
+++ .bash_functions.sh
@@ -0,0 +1,15 @@
+
+# Define GIT PS1
+__my_git_ps1 ()
+{
+    local b="$(git symbolic-ref HEAD 2>/dev/null)";
+    if [ -n "$b" ]; then
+        printf ":(%s)" "${b##refs/heads/}";
+    fi
+}
+
+# Define function to grab files captured by grep
+grepf()
+{
+    grep -r "$1" . | sed 's/:.*//' | uniq
+}
