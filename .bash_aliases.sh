e85e4ae1d5adfb25887c889be973276dde0f2000
shanbak <me@me.com>
2013-05-18 22:09:45 +0100

    -xmonad, +i3status, +tig

--- .bash_aliases.sh
+++ .bash_aliases.sh
@@ -14,6 +14,7 @@ export GLOG_logtostderr=1
 #-----------------
 alias rm='rm -I'
 alias l='ls -lh --color=always'
+alias ld='ls -ld'
 alias ll='ls -hla --color=always'
 alias ls='ls -h --color=always'
 alias grep='grep -n --color'
@@ -25,6 +26,7 @@ alias cclean='rm -rf logs/ artifact/ AS5_64/ CMakeFiles/ cmake_install.cmake Mak
 #-----------------
 alias ..='cd ..'
 alias ...='cd ../..'
+alias qa='cd /home/me/workspace'
 
 #-----------------
 # admin
@@ -42,6 +44,7 @@ alias cvsd='export CVSROOT=$CVSROOT_DEV'
 alias cvso='export CVSROOT=$CVSROOT_OPENBET'
 alias vall='vim -p src/*.cpp'
 alias vallt='vim -p test/*.cpp'
+alias tig='tig --all'
 
 alias xephyr='Xephyr -ac -kb -screen 1074x1889 -br -reset -terminate :1 &'
 alias gdms='DISPLAY=:1.0 gnome-session --session=2d-gnome'
--- .bashrc
+++ .bashrc
@@ -22,6 +22,7 @@ xterm*|rxvt*)
     PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME}: ${PWD/$HOME/~}\007"'
     ;;
 *)
+    xset -b
     PS1='\[\033[1;37m\]\u:\h\[\033[0m\]\[\033[37m\]$(__my_git_ps1)\[\033[0m\]:\W\$:'
     ;;
 esac
